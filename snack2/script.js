d2f5035c5214895138367c08d04ed73d3375fd1d
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-14 17:47:29 +0100

    snack2

---
+++ snack2/script.js
@@ -0,0 +1,8 @@
+let parola1 = prompt("inserisci parola");
+let parola2 = prompt("inserisci parola");
+
+if(parola1>parola2){
+console.log(parola1)
+}else if(parola1<parola2){
+console.log(parola2)
+}
