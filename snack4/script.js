775371c32bdd96608c2790ab9ebbf3bc9ee461b8
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-14 17:57:03 +0100

    snack4

---
+++ snack4/script.js
@@ -0,0 +1,9 @@
+let arrayNomiInvitati = ["Nick Carraway", "Daisy Buchanan", "Meyer Wolfsheim","Jordan Baker"];
+
+let nomeUtente = prompt("verifica invito");
+
+if(!arrayNomiInvitati){
+console.log("il nuo nome non fa parte degli invitati");
+}else{
+    console.log("fai parte degli invitati");
+}
