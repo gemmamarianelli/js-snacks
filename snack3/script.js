3de012bcff827807d0c2feb5ae91a2d35db4ff8d
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-14 17:48:29 +0100

    snack2

---
+++ snack3/script.js
@@ -0,0 +1,12 @@
+let numero = prompt("inserisci numero");
+let numero1 = prompt("inserisci numero");
+let numero2 = prompt("inserisci numero");
+let numero3 = prompt("inserisci numero");
+let numero4 = prompt("inserisci numero");
+let numero5 = prompt("inserisci numero");
+let numero6 = prompt("inserisci numero");
+let numero7 = prompt("inserisci numero");
+let numero8 = prompt("inserisci numero");
+let numero9 = prompt("inserisci numero");
+
+console.log(numero+=numero1+=numero2+=numero3+=numero4+=numero5+=numero6+=numero7+=numero8+=numero9)
